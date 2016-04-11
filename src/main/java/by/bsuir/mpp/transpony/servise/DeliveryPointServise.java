4dac168451268b7c7534831ce17f717c9f066383
Vadim Vidnichuk <vadim3111@tut.by>
2016-04-11 16:42:37 +0300

    add service deliveryPoint, Provider, Receiver

---
+++ src/main/java/by/bsuir/mpp/transpony/servise/DeliveryPointServise.java
@@ -0,0 +1,55 @@
+package by.bsuir.mpp.transpony.servise;
+
+import by.bsuir.mpp.transpony.dao.DaoException;
+import by.bsuir.mpp.transpony.dao.DeliveryPointDao;
+import by.bsuir.mpp.transpony.dao.factory.DaoFactory;
+import by.bsuir.mpp.transpony.entity.DeliveryPoint;
+
+import java.util.List;
+
+public class DeliveryPointServise {
+    private static final DeliveryPointDao dao = DaoFactory.getDaoFactory().getDeliveryPointDao();
+
+    public static DeliveryPoint getForIndex(Integer index) throws ServiceException {
+        try {
+           return dao.getForIndex(index);
+        } catch (DaoException e) {
+            throw new ServiceException("can't get for index delivery point.", e);
+        }
+    }
+    public static List<DeliveryPoint> getAllForReceiver(Integer index) throws ServiceException {
+        try {
+            return dao.getAllForReceiver(index);
+        } catch (DaoException e) {
+            throw new ServiceException("can't get all for index receiver.", e);
+        }
+
+    }
+
+    public static Integer add(DeliveryPoint deliveryPoint) throws ServiceException {
+        try {
+            return dao.add(deliveryPoint);
+        } catch (DaoException e) {
+            throw new ServiceException("can't add delivery point.", e);
+        }
+
+    }
+    public static void update(DeliveryPoint deliveryPoint) throws ServiceException {
+        try {
+            dao.update(deliveryPoint);
+        } catch (DaoException e) {
+            throw new ServiceException("can't update delivery point.", e);
+        }
+
+    }
+    public static void delete(DeliveryPoint deliveryPoint) throws ServiceException {
+        try {
+            dao.delete(deliveryPoint);
+        } catch (DaoException e) {
+            throw new ServiceException("can't delete delivery point.", e);
+        }
+
+    }
+
+
+}
---
+++ src/main/java/by/bsuir/mpp/transpony/servise/ReceiverService.java
@@ -0,0 +1,64 @@
+package by.bsuir.mpp.transpony.servise;
+
+
+import by.bsuir.mpp.transpony.dao.DaoException;
+import by.bsuir.mpp.transpony.dao.ReceiverDao;
+import by.bsuir.mpp.transpony.dao.factory.DaoFactory;
+import by.bsuir.mpp.transpony.entity.DeliveryPoint;
+import by.bsuir.mpp.transpony.entity.Receiver;
+
+import java.util.List;
+
+public class ReceiverService {
+    private static final ReceiverDao dao = DaoFactory.getDaoFactory().getReceiverDao();
+
+    public static List<Receiver> getAll() throws ServiceException {
+        try {
+            return dao.getAll();
+        } catch (DaoException e) {
+            throw new ServiceException("", e);
+        }
+    }
+
+    public static Integer add(Receiver receiver) throws ServiceException {
+        try {
+            return dao.add(receiver);
+        } catch (DaoException e) {
+            throw new ServiceException("", e);
+        }
+
+    }
+    public static  void delete(Receiver receiver) throws ServiceException {
+        try {
+            dao.delete(receiver);
+        } catch (DaoException e) {
+            throw new ServiceException("", e);
+        }
+
+    }
+    public static void update(Receiver receiver) throws ServiceException {
+        try {
+            dao.update(receiver);
+        } catch (DaoException e) {
+            throw new ServiceException("", e);
+        }
+
+    }
+
+    public static List<DeliveryPoint> getDeliveryPointForReceiver(Integer index) throws ServiceException {
+        try {
+            return dao.getDeliveryPointForReceiver(index);
+        } catch (DaoException e) {
+            throw new ServiceException("", e);
+        }
+
+    }
+
+    public static Receiver getForIndex(Integer index) throws ServiceException {
+        try {
+            return dao.getForIndex(index);
+        } catch (DaoException e) {
+            throw new ServiceException("", e);
+        }
+    }
+}
